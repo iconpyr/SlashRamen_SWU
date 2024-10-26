2dc411d2aefde7992119dfd10a762d282c89dbda
iconpyr <iconpiyorod@gmail.com>
2024-10-26 16:32:24 +0700

    add font and some style

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 "use client"; // This is a client component
+import Head from 'next/head'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
@@ -20,85 +21,96 @@ export default function Home() {
   }
 
   return (
-    <div className="max-w-7xl mx-auto h-screen bg-white">
-      <div className="text-4xl text-green-800 mb-2">Slash Ramen</div>
+    <div>
 
-      <div className="grid grid-cols-3 gap-4">
-        <NewRamenButton
-          ramenname = "Ramen & Half-boiled salted egg"
-          image="/images/mar22_ramen_12_e4tdxz.webp"
-          onAddToCart={() => handleAddToCart("Ramen & Half-boiled salted egg")}
-        />
-        <NewRamenButton
-          ramenname = "Tonkotsu Ramen"
-          image="/images/tonkotsuramenfront.jpg"
-          onAddToCart={() => handleAddToCart("Tonkotsu Ramen")}
-        />
-        <NewRamenButton
-          ramenname = "Shoyu Ramen"
-          image="/images/tonkotsuramenfront.jpg"
-          onAddToCart={() => handleAddToCart("Shoyu Ramen")}
-        />
-      </div>
+      <div className="max-w-6xl p-2 mx-auto h-screen bg-tea-200">
+        <div className="text-4xl text-black font-bold mb-2">Slash Ramen</div>
 
-      <div className="grid grid-cols-6 gap-4 pt-4">
-        <OptionBox
-          title="Extra Chashu Pork"
-          price="70฿"
-          disabled={!ramenButton1}
-        />
-        <OptionBox
-          title="Marinated Egg"
-          price="70฿"
-          disabled={!ramenButton2}
-        />
-        <OptionBox
-          title="Extra Chashu Pork"
-          price="70฿"
-          disabled={!ramenButton3}
-        />
-        <OptionBox title="Extra Chashu Pork" price="70฿" disabled={true} />
-        <OptionBox title="Extra Chashu Pork" price="70฿" disabled={true} />
-        <OptionBox title="Extra Chashu Pork" price="70฿" disabled={true} />
-      </div>
+        <div className="grid grid-cols-3 gap-4">
+          <NewRamenButton
+            price = "180"
+            ramenname="Ramen & Half-boiled salted egg"
+            image="/images/mar22_ramen_12_e4tdxz.webp"
+            onAddToCart={() =>
+              handleAddToCart("Ramen & Half-boiled salted egg")
+            }
+          />
+          <NewRamenButton
+            price = "170"
+            ramenname="Tonkotsu Ramen"
+            image="/images/tonkotsuramenfront.jpg"
+            onAddToCart={() => handleAddToCart("Tonkotsu Ramen")}
+          />
+          <NewRamenButton
+            price = "190"
+            ramenname="Shoyu Ramen"
+            image="/images/tonkotsuramenfront.jpg"
+            onAddToCart={() => handleAddToCart("Shoyu Ramen")}
+          />
+        </div>
 
-      <div className="grid grid-cols-6 gap-4 pt-4">
-        <OptionBox title="3X Spicy" price="30฿" disabled={!ramenButton2} />
-        <OptionBox title="2X Spicy" price="20฿" disabled={!ramenButton3} />
-        <OptionBox title="1X Spicy" price="" disabled={!ramenButton1} />
-        <OptionBox title="" price="" disabled={true} />
-        <OptionBox title="" price="" disabled={true} />
-        <OptionBox title="" price="" disabled={true} />
-      </div>
+        <div className="grid grid-cols-6 gap-4 pt-4">
+          <OptionBox
+            title="Extra Chashu Pork"
+            price="70฿"
+            disabled={!ramenButton1}
+          />
+          <OptionBox
+            title="Marinated Egg"
+            price="70฿"
+            disabled={!ramenButton2}
+          />
+          <OptionBox
+            title="Extra Chashu Pork"
+            price="70฿"
+            disabled={!ramenButton3}
+          />
+          <OptionBox title="Extra Chashu Pork" price="70฿" disabled={true} />
+          <OptionBox title="Extra Chashu Pork" price="70฿" disabled={true} />
+          <OptionBox title="Extra Chashu Pork" price="70฿" disabled={true} />
+        </div>
 
-      <div className="pt-6">
-        <div className="grid grid-cols-3 gap-4 h-44">
-          <div className="col-span-2 border-2 rounded-2xl bg-gray-200 p-4 flex-space text-green-800">
-            <div>
-            <FontAwesomeIcon icon={faShoppingCart} size="xl"/>
-            </div>
-            <div className="text-green-800 font-bold "></div>
-            {cart.length === 0 ? (
-              <div className="text-gray-400 pt-2">Cart is empty</div>
-            ) : (
-              <ul className="text-green-800">
-                {cart.map((item, index) => (
-                  <li key={index} className="flex justify-between items-center">
-                    {item}
-                    <button
-                      className="text-red-500 bg-white border-2 px-2 py-1 rounded-md hover:bg-red-500 hover:text-white transition-all"
-                      onClick={() => handleRemoveFromCart(index)}
+        <div className="grid grid-cols-6 gap-4 pt-4">
+          <OptionBox title="3X Spicy" price="30฿" disabled={!ramenButton2} />
+          <OptionBox title="2X Spicy" price="20฿" disabled={!ramenButton3} />
+          <OptionBox title="1X Spicy" price="" disabled={!ramenButton1} />
+          <OptionBox title="" price="" disabled={true} />
+          <OptionBox title="" price="" disabled={true} />
+          <OptionBox title="" price="" disabled={true} />
+        </div>
+
+        <div className="pt-6">
+          <div className="grid grid-cols-3 gap-4 h-44">
+            <div className="col-span-2 border-2 rounded-2xl bg-gray-200 p-4 flex-space text-green-800">
+              <div>
+                <FontAwesomeIcon icon={faShoppingCart} size="xl" />
+              </div>
+              <div className="text-green-800 font-bold "></div>
+              {cart.length === 0 ? (
+                <div className="text-gray-400 pt-2">Cart is empty</div>
+              ) : (
+                <ul className="text-green-800">
+                  {cart.map((item, index) => (
+                    <li
+                      key={index}
+                      className="flex justify-between items-center"
                     >
-                      Cancel
-                    </button>
-                  </li>
-                ))}
-              </ul>
-            )}
-          </div>
+                      {item}
+                      <button
+                        className="text-red-500 bg-white border-2 px-2 py-1 rounded-md hover:bg-red-500 hover:text-white transition-all"
+                        onClick={() => handleRemoveFromCart(index)}
+                      >
+                        Cancel
+                      </button>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
 
-          <div className="cursor-pointer text-xl bg-green-600 hover:bg-green-200 transition-all duration-300 block h-20 content-center text-center rounded-2xl">
-            Pay Button
+            <div className="cursor-pointer text-xl bg-green-600 hover:bg-green-200 transition-all duration-300 block h-20 content-center text-center rounded-2xl">
+              Pay Button
+            </div>
           </div>
         </div>
       </div>
@@ -121,33 +133,31 @@ const OptionBox = (props) => {
     </div>
   );
 };
-
 const NewRamenButton = (props) => {
   return (
-    <div className="flex rounded-xl h-56 overflow-hidden shadow-md">
+    <div className="flex flex-col h-64 rounded-xl overflow-hidden shadow">
       <div
-        className="bg-red-200 w-1/2"
+        className="h-4/6 bg-cover"
         style={{ backgroundImage: `url(${props.image})` }}
       ></div>
-      <div className="flex flex-col bg-gray-200 w-1/2">
-        <div className="text-md font-bold text-green-800 p-3 h-1/2">
-          {props.ramenname}
+      <div className="flex h-2/6">
+        <div className="flex flex-col bg-red-600 w-8/12 p-2 justify-between">
+          <p className="text-md text-white">{props.ramenname}</p>
+          <p className="text-3xl text-right font-bold text-white">{props.price}฿</p>
         </div>
-        <div className="flex text-green-800">
-          <div className="p-3 ">
-            <p className="text-2xl font-bold">180</p>
-            Baht
-          </div>
-          <div className="pt-3.5">
-            <button
-              className="text-md py-2 px-6 border-8 bg-green-600 border-l-green-400 border-t-green-300 border-b-green-800 border-r-green-700"
-              onClick={props.onAddToCart}
-            >
-              <FontAwesomeIcon icon={faShoppingCart}/>
-            </button>
-          </div>
+        <div className="flex w-4/12 bg-amber-200 items-center justify-center">
+          <button
+            className="rounded-lg text-md py-1 px-2 border-8 bg-yellow-400 border-l-yellow-300 border-t-yellow-200 border-b-yellow-600 border-r-yellow-500"
+            onClick={props.onAddToCart}
+          >
+            {/* <div className="bg-red-500 py-1 px-2 rounded-full"> */}
+            <FontAwesomeIcon icon={faShoppingCart} />{" "}
+            <span className="font-bold text-red-500">Order</span>
+            {/* </div> */}
+          </button>
         </div>
       </div>
     </div>
   );
 };
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,12 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'machineBody': '#f9f6ef',
+        'tea': {  DEFAULT: '#C0B9AA',  50: '#FFFFFF',  100: '#FFFFFF',  200: '#F4F3F0',  300: '#E3E0D9',  400: '#D1CCC1',  500: '#C0B9AA',  600: '#A89E8A',  700: '#8F846A',  800: '#6F6652',  900: '#4F493B',  950: '#3F3A2F'},
       },
+      fontFamily: {
+        "lato": ['Lato', 'sans-serif']
+      }
     },
   },
   plugins: [],
